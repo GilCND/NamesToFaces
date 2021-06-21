bd886d1ee6d411e09185942685ceedeaf7f09404
Felipe Gil <felipe.gil@redspace.com>
2021-06-21 14:53:28 -0300

    skeleton

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nameToFaces
+//
+//  Created by Felipe Gil on 2021-06-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
